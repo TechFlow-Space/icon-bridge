da7ea53e7d684da6c33af6587663aa614ef3b420
hungrywolf23 <nobelpokharel6@gmail.com>
2023-06-01 12:34:03 +0545

    added tests

--- smartpy/bmc/test/integration_test.py
+++ smartpy/bmc/test/integration_test.py
@@ -1,13 +1,19 @@
 import smartpy as sp
 
-BMCPeriphery = sp.io.import_script_from_url("file:./contracts/src/bmc_periphery.py")
 BMCManagement = sp.io.import_script_from_url("file:./contracts/src/bmc_management.py")
-ParseAddress = sp.io.import_script_from_url("file:./contracts/src/parse_address.py")
-Helper= sp.io.import_script_from_url("file:./contracts/src/helper.py")
+BMCPeriphery = sp.io.import_script_from_url("file:./contracts/src/bmc_periphery.py")
+BMCHelper = sp.io.import_script_from_url("file:../bts/contracts/src/helper.py")
+ParseAddress = sp.io.import_script_from_url("file:../bts/contracts/src/parse_address.py")
+
+BTSCore = sp.io.import_script_from_url("file:../bts/contracts/src/bts_core.py")
+BTSOwnerManager = sp.io.import_script_from_url("file:../bts/contracts/src/bts_owner_manager.py")
+BTSPeriphery = sp.io.import_script_from_url("file:../bts/contracts/src/bts_periphery.py")
 
 
 
-@sp.add_test("flow test")
+
+
+@sp.add_test("BMCManagementTest")
 def test():
     sc = sp.test_scenario()
 
@@ -16,59 +22,181 @@ def test():
     creator = sp.test_account("Creator")
     jack = sp.test_account("Jack")
     bob = sp.test_account("Bob")
-    helper=sp.test_account('helper')
-    parse_contract = sp.test_account("Parser")
-
+    creator2 = sp.test_account("creator2")
     service1_address = sp.test_account("service1_address")
-    block_interval = sp.nat(2)
-    _max_aggregation = sp.nat(3)
-    delay_limit = sp.nat(2)
-    link = sp.string('btp://77.tezos/tz1e2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnA')
+    service2_address = sp.test_account("service2_address")
 
 
-    def deploy_bmcManagement_contract():
-     bmcManagement_contract = BMCManagement.BMCManagement(creator.address,btshelpercontract.address)
-     return bmcManagement_contract
-    
-    def deploy_bmcperiphery_contract():
-        bmcperiphery_contract = BMCPeriphery.BMCPreiphery(bmcManagement_contract.address,btshelpercontract.address, btsparsecontract.address, creator.address)
-        return bmcperiphery_contract
-    
-    def deploy_parsecontract():
-       btsparsecontract = ParseAddress.ParseAddress()
-       return btsparsecontract
-    
-    def deploy_helper():
-       btshelper = Helper.Helper()
-       return btshelper
-    
-    btshelpercontract = deploy_helper()
-    sc+= btshelpercontract
+    # deploy BMCManagement contract
 
-    btsparsecontract = deploy_parsecontract()
-    sc+= btsparsecontract
+    helper_contract = deploy_helper_contract()
+    sc += helper_contract
 
-    bmcManagement_contract = deploy_bmcManagement_contract()
+    bmcManagement_contract = deploy_bmcManagement_contract(sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"), helper_contract.address)
     sc += bmcManagement_contract
+
+    parse_address = deploy_parse_address()
+    sc += parse_address
+
+    bmcPeriphery_contract = deploy_bmcPeriphery_contract(bmcManagement_contract.address, helper_contract.address, parse_address.address)
+    sc += bmcPeriphery_contract
+
+    bts_owner_manager = deploy_btsOwnerManager_Contract()
+    sc += bts_owner_manager
+    btsCore_contract = deploy_btsCore_contract(bts_owner_manager.address)
+    sc += btsCore_contract
+
+
+    bts_periphery = deploy_btsPeriphery_Contract(btsCore_contract.address, helper_contract.address, parse_address.address, bmcPeriphery_contract.address)
+    sc += bts_periphery
+
+    #set bmc periphery
+    bmcManagement_contract.set_bmc_periphery(bmcPeriphery_contract.address).run(sender=sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"))
+    #set bmc_btp_address(netwrk address)
+    bmcManagement_contract.set_bmc_btp_address("NetXnHfVqm9iesp.tezos").run(sender=sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"))
+    #update_bts_periphery
+    btsCore_contract.update_bts_periphery(bts_periphery.address).run(sender=sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"))
+
+    #add_service
+    svc1 = sp.string("bts")
+    bmcManagement_contract.add_service(sp.record(addr=bts_periphery.address, svc=svc1)).run(sender=sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"))
+
+    #add_route
+    dst = "btp://0x7.icon/cxff8a87fde8971a1d10d93dfed3416b0a6258dest"
+    next_link = "btp://0x7.icon/cxff8a87fde8971a1d10d93dfed3416b0a6258d67b"
+    bmcManagement_contract.add_route(sp.record(dst=dst, link=next_link)).run(sender=sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"))
+
+    # bmcManagement_contract.add_route(sp.record(dst="btp://NetXnHfVqm9iesp.tezos/cxff8a87fde8971a1d10d93dfed3416b0a6258dest", link="btp://NetXnHfVqm9iesp.tezos/cxff8a87fde8971a1d10d93dfed3416b0a6258d67b")).run(sender=sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"))
+
+
+    #add_link
+    bmcManagement_contract.add_link("btp://0x7.icon/cxff8a87fde8971a1d10d93dfed3416b0a6258d67b").run(sender=sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"))
+
+
+    # #remove_link
+    # bmcManagement_contract.add_link("btp://0x7.icon/cxff8a87fde8971a1d10d93dfed3416b0a6258dead").run(sender=sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"))
+    # bmcManagement_contract.remove_link("btp://0x7.icon/cxff8a87fde8971a1d10d93dfed3416b0a6258dead").run(sender=sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"))
+
+
+    # #set_link_rx_height
+    # link = sp.string('btp://0x7.icon/cxff8a87fde8971a1d10d93dfed3416b0a6258d67b')
+    # height = sp.nat(2)
+    # bmcManagement_contract.set_link_rx_height(link=link, height=height).run(sender=sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"))
+
+    #add_relay
+    bmcManagement_contract.add_relay(sp.record(link="btp://0x7.icon/cxff8a87fde8971a1d10d93dfed3416b0a6258d67b", addr=sp.set([sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9")]))).run(sender=sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"))
+
+    # #test 1: Test of add to blacklist function
+    # # bts_periphery.add_to_blacklist({0:"notaaddress"}).run(sender=bts_periphery.address,valid=False, exception="InvalidAddress") # invalid address 
+    # bts_periphery.add_to_blacklist({0:"tz1e2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW"}).run(sender=bts_periphery.address,valid=True) #add a address to blacklist
+    # # bts_periphery.add_to_blacklist({0:"tz1e2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW"}).run(sender=bts_periphery.address,valid=True)  # can be called twice 
+    # # bts_periphery.add_to_blacklist({0:"tz1e2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW"}) .run(sender=alice.address,valid=False,exception ="Unauthorized")# only btsperiphery contract call this function
+    # # bts_periphery.add_to_blacklist({0:'tz1ZZZZZZZZZZZZZZZZZZZZZZZZZZZZNkiRg'}).run(sender=bts_periphery.address,valid=False,exception='InvalidAddress')#invalid address
+    # # sc.verify(bts_periphery.data.blacklist[sp.address("tz1e2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW")] == True) # checking the blacklist[] map
+
+    # transfer_native_coin
+    # bts_periphery.set_token_limit(
+    #     sp.record(
+    #         coin_names=sp.map({0: "btp-NetXnHfVqm9iesp.tezos-XTZ"}),
+    #         token_limit=sp.map({0: 115792089237316195423570985008687907853269984665640564039457584007913129639935})
+    #     )
+    # ).run(sender = btsCore_contract.address)
+    # btsCore_contract.transfer_native_coin("btp://0x7.icon/cx4419cb43f1c53db85c4647e4ef0707880309726d").run(sender= sp.address("tz1e2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW"), amount=sp.tez(30), valid=False, exception="FailCheckTransfer")
+
+    btsCore_contract.transfer_native_coin("btp://0x7.icon/cx4419cb43f1c53db85c4647e4ef0707880309726d").run(sender= sp.address("tz1e2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW"), amount=sp.tez(30))
+
+    # handle_relay_message
+    msg=sp.bytes("0xf8e1f8dfb8ddf8db01b8d3f8d1f8cfb8406274703a2f2f4e6574586e486656716d39696573702e74657a6f732f4b54314657446a4338435941777268424a6d43355554596d594a544a6457776f3447676203b88af888b8396274703a2f2f3078372e69636f6e2f637833643436306163643535356336373034303566396562323934333833356366643132326662323938b8406274703a2f2f4e6574586e486656716d39696573702e74657a6f732f4b54314657446a4338435941777268424a6d43355554596d594a544a6457776f344767628362747381ff84c328f8008400886513")
+    prev=sp.string("btp://0x7.icon/cxff8a87fde8971a1d10d93dfed3416b0a6258d67b")
+    bmcPeriphery_contract.handle_relay_message(sp.record(prev=prev, msg=msg)).run(sender=sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"))
    
 
-    # deploy BMCPeriphery contract
-    bmcperiphery_contract = deploy_bmcperiphery_contract()
-    sc += bmcperiphery_contract
+    # #test 2 : Test of remove from blacklist function
+    # # bts_periphery.remove_from_blacklist({0:'notaaddress'}).run(sender=bts_periphery.address,valid=False, exception="InvalidAddress") # invalid address 
+    # # bts_periphery.remove_from_blacklist({0:'tz1d2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW'}).run(sender=bts_periphery.address,valid=False, exception="UserNotFound") # address not black-listed
+    # # bts_periphery.add_to_blacklist({0:'tz1d2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW'}).run(sender=bts_periphery.address) # adding to blacklist
+    # bts_periphery.remove_from_blacklist({0:'tz1e2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW'}).run(sender=bts_periphery.address) # valid process
+    # # bts_periphery.remove_from_blacklist({0:'tz1d2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW'}).run(sender=bts_periphery.address,valid=False ,exception ='UserNotFound') # cannot remove from blacklist twice
+    # # bts_periphery.add_to_blacklist({0:'tz1d2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW'}).run(sender=bts_periphery.address) # adding to blacklist
+    # # bts_periphery.remove_from_blacklist({0:'tz1d2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW'}).run(sender=bts_periphery.address) # can only be called from btseperiphery contract
+    # # bts_periphery.remove_from_blacklist({0:'tz1d2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW'}).run(sender=alice.address, valid=False, exception ="Unauthorized") # can only be called from btseperiphery contract
+
+    # # transfer_native_coin
+    # bts_periphery.set_token_limit(
+    #     sp.record(
+    #         coin_names=sp.map({0: "btp-NetXnHfVqm9iesp.tezos-XTZ"}),
+    #         token_limit=sp.map({0: 115792089237316195423570985008687907853269984665640564039457584007913129639935})
+    #     )
+    # ).run(sender = btsCore_contract.address)
+    # btsCore_contract.transfer_native_coin("btp://0x7.icon/cx4419cb43f1c53db85c4647e4ef0707880309726d").run(sender= sp.address("tz1e2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW"), amount=sp.tez(30))
+
+
+    # # transfer_native_coin
+    # bts_periphery.set_token_limit(
+    #     sp.record(
+    #         coin_names=sp.map({0: "btp-NetXnHfVqm9iesp.tezos-XTZ"}),
+    #         token_limit=sp.map({0: 5})
+    #     )
+    # ).run(sender = btsCore_contract.address)
+    # btsCore_contract.transfer_native_coin("btp://0x7.icon/cx4419cb43f1c53db85c4647e4ef0707880309726d").run(sender= sp.address("tz1e2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW"), amount=sp.tez(30),  valid=False, exception="FailCheckTransfer")
+
+    # # bmc_periphery get_status
+    # sc.verify_equal(bmcPeriphery_contract.get_status("btp://0x7.icon/cxff8a87fde8971a1d10d93dfed3416b0a6258d67b"), sp.record(current_height = 0, rx_height = 2, rx_seq = 0, tx_seq = 6))
+
+    
+
+    # # #test 3 : set token  limit
+    # # bts_periphery.set_token_limit(sp.record(coin_names = {0:"Tok2" , 1:'BB'} ,token_limit ={0:sp.nat(5),1:sp.nat(2)})).run(sender=alice.address,valid=False,exception='Unauthorized') #can only be called from btsperiphery contract
+    # # bts_periphery.set_token_limit(sp.record(coin_names = {0:"Tok2" , 1:'BB'} ,token_limit ={0:sp.nat(5),1:sp.nat(2)})).run(sender=bts_periphery.address) #set token limit for Tok2 coin to 5 and BB coin to 2
+    # # sc.verify(bts_periphery.data.token_limit["Tok2"] == sp.nat(5))#test of token_limit for tok2 token
+    # # bts_periphery.set_token_limit(sp.record(coin_names = {0:"Tok2" , 1:'BB'} ,token_limit ={0:sp.nat(5)} )).run(valid=False,exception='InvalidParams',sender=bts_periphery.address) #invalid parameters
+    # # #cannot set more than 15 token limit at once
+    # # bts_periphery.set_token_limit(sp.record(coin_names = {0:"Tok2" , 1:'BB'} ,token_limit ={0:sp.nat(15),1:sp.nat(22)})).run(sender=bts_periphery.address) #can modify already set data
+    # # sc.verify(bts_periphery.data.token_limit["BB"] == sp.nat(22))#test of token_limit for tok2 token
+
+    # # handle_relay_message
+    # msg=sp.bytes("0xf90157f90154b90151f9014e01b90145f90142f9013fb8406274703a2f2f4e6574586e486656716d39696573702e74657a6f732f4b5431454e5a76546f507838374c68756f774a315669786a6a715168536e597263594c6907b8faf8f8b8396274703a2f2f3078372e69636f6e2f637864633238393434343037363539393733666539393438376437356335646433326337396265303533b8406274703a2f2f4e6574586e486656716d39696573702e74657a6f732f4b5431454e5a76546f507838374c68756f774a315669786a6a715168536e597263594c698362747303b874f87200b86ff86daa687839643138316431336634376335616165353535623730393831346336623232393738373937363139a4747a3165703766664b7351434e64676e6b504443566e566b67626d465a50386d464e3147dcdb906274702d3078372e69636f6e2d4943588900d71b0fe0a28e000084008502ba")
+    # prev=sp.string("btp://0x7.icon/cxff8a87fde8971a1d10d93dfed3416b0a6258d67b")
+    # bmcPeriphery_contract.handle_relay_message(sp.record(prev=prev, msg=msg)).run(sender=sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"))
+
+    # # #set_fee_ratio
+    # # btsCore_contract.set_fee_ratio(name=sp.string("btp-NetXnHfVqm9iesp.tezos-XTZ"),fee_numerator=sp.nat(100),fixed_fee=sp.nat(450)).run(sender=sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"))
 
 
 
-    bmcManagement_contract.set_bmc_periphery(bmcperiphery_contract.address).run(sender=creator)
-    bmcManagement_contract.set_bmc_btp_address("tezos.77").run(sender=creator)
-    svc1 = sp.string("service1")
-    dst = "btp://77.tezos/tz1e2HPzZWBsuExFSM4XDBtQiFnaUB5hDEST"
-    bmcManagement_contract.add_service(sp.record(addr=service1_address.address, svc=svc1)).run(sender=creator)
-    bmcManagement_contract.add_route(sp.record(dst = "btp://77.tezos/tz1e2HPzZWBsuExFSM4XDBtQiFnaUB5hDEST", link = "btp://77.tezos/tz1e2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW")).run(sender=creator)
-    #add-link error in local environment due to hard-coded HELPER_CONTRACT_ADDRESS in rlp_encode_contract line 4
-    #bmcManagement_contract.add_link("btp://77.tezos/tz1e2HPzZWBsuExFSM4XDBtQiFnaUB5hiPnW").run(sender=creator)
-    #bmcManagement_contract.add_relay(sp.record(link=link, addr=sp.set([sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9")]))).run(sender=creator)
-    #bmcManagement_contract.set_link(sp.record(_link=link, block_interval=block_interval,_max_aggregation=_max_aggregation, delay_limit=delay_limit)).run(sender=creator)
+
+def deploy_bmcManagement_contract(owner, helper):
+    bmcManagement_contract = BMCManagement.BMCManagement(owner, helper)
+    return bmcManagement_contract
+
+def deploy_bmcPeriphery_contract(bmc_addres, helper, parse):
+    owner = sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9")
+    bmcPeriphery_contract = BMCPeriphery.BMCPreiphery(bmc_addres, helper, parse, owner)
+    return bmcPeriphery_contract
+
+def deploy_helper_contract():
+    helper_contract = BMCHelper.Helper()
+    return helper_contract
 
 
+def deploy_parse_address():
+    parse_address = ParseAddress.ParseAddress()
+    return parse_address
 
-    #sc.show(bmcperiphery_contract.get_status(link))
+
+def deploy_btsCore_contract(bts_OwnerManager_Contract):
+    btsCore_contract = BTSCore.BTSCore(
+        owner_manager=bts_OwnerManager_Contract,
+        _native_coin_name="btp-NetXnHfVqm9iesp.tezos-XTZ",
+        _fee_numerator=sp.nat(100),
+        _fixed_fee=sp.nat(450)
+    )
+    return btsCore_contract
+
+def deploy_btsOwnerManager_Contract():
+    bts_OwnerManager_Contract = BTSOwnerManager.BTSOwnerManager(sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"))
+    return bts_OwnerManager_Contract
+
+def deploy_btsPeriphery_Contract(core_address, helper, parse, bmc):
+    btsPeriphery_Contract = BTSPeriphery.BTPPreiphery(bmc_address= bmc, bts_core_address=core_address, helper_contract=helper, parse_address=parse, owner_address=sp.address("tz1XGbmLYhqcigxFuBCJrgyJejnwkySE4Sk9"), native_coin_name="btp-NetXnHfVqm9iesp.tezos-XTZ")
+    return btsPeriphery_Contract
